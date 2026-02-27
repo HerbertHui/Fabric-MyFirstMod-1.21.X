3d5d2b7fe0594c31e3aaecb2c80ff08e31528712
HerbertHui <899990306@qq.com>
2026-02-27 20:52:31 +0800

    Two items have been added.

--- src/main/java/net/herbert/myfirstmod/MyFirstMod.java
+++ src/main/java/net/herbert/myfirstmod/MyFirstMod.java
@@ -2,6 +2,7 @@ package net.herbert.myfirstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.herbert.myfirstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class MyFirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/herbert/myfirstmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.herbert.myfirstmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.herbert.myfirstmod.MyFirstMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MyFirstMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MyFirstMod.LOGGER.info("Registering Mod Items for " + MyFirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
