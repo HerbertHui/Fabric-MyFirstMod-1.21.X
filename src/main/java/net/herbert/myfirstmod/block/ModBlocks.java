bd448aed41670d806f5f303ca8b4a3035871dd14
HerbertHui <899990306@qq.com>
2026-02-27 21:47:51 +0800

    A block has been added.

---
+++ src/main/java/net/herbert/myfirstmod/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.herbert.myfirstmod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.herbert.myfirstmod.MyFirstMod;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+            new Block(AbstractBlock.Settings.create().strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
+
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(MyFirstMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(MyFirstMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        MyFirstMod.LOGGER.info("Registering Mod Blocks for " + MyFirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.PINK_GARNET_BLOCK);
+        });
+    }
+}
