feb65d7ff5f6396f45ac92b8de4673549c72e2c6
HerbertHui <899990306@qq.com>
2026-02-28 15:54:17 +0800

    A magic block has been added.

---
+++ src/main/java/net/herbert/myfirstmod/block/custom/MagicBlock.java
@@ -0,0 +1,39 @@
+package net.herbert.myfirstmod.block.custom;
+
+import net.herbert.myfirstmod.item.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class MagicBlock extends Block {
+    public MagicBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        world.playSound(player, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 5.0F, 1.0F);
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        if(entity instanceof ItemEntity itemEntity) {
+            if(itemEntity.getStack().getItem() == Items.COBBLESTONE) {
+                itemEntity.setStack(new ItemStack(Items.GOLD_INGOT, itemEntity.getStack().getCount()));
+            }
+        }
+
+        super.onSteppedOn(world, pos, state, entity);
+    }
+}
