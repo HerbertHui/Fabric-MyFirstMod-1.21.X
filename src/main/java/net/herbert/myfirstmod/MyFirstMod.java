d58e3afa4cbea3b0a9e59a958e97f4db3fbd131f
HerbertHui <899990306@qq.com>
2026-02-28 20:39:38 +0800

    A new food and a new fuel have been added.

--- src/main/java/net/herbert/myfirstmod/MyFirstMod.java
+++ src/main/java/net/herbert/myfirstmod/MyFirstMod.java
@@ -2,6 +2,7 @@ package net.herbert.myfirstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.herbert.myfirstmod.block.ModBlocks;
 import net.herbert.myfirstmod.item.ModItemGroups;
 import net.herbert.myfirstmod.item.ModItems;
@@ -18,5 +19,7 @@ public class MyFirstMod implements ModInitializer {
 
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
+
+        FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600); // Refer to the fuelTime in AbstractFurnaceBlockEntity.java
 	}
 }
